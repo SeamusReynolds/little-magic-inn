ff9cd5b0f3e56cb99dab43232651f26192fef0f1
Seamus Reynolds <seamus.reynolds@gmail.com>
2018-04-20 07:59:56 -0700

    Adding /api/v1 to the routes so that we can always upgrade to new versions while leaving old endpoints intact for users if necessary.

--- src/main/java/app/util/Path.java
+++ src/main/java/app/util/Path.java
@@ -5,8 +5,8 @@ public class Path {
     // The @Getter methods are needed in order to access
     // the variables from Velocity Templates
     public static class Endpoint {
-        public static final String AVAILABLE_ROOMS = "/availableRooms";
-        public static final String CREATE_BOOKING = "/booking";
-        public static final String CLEANING_SCHEDULE = "/cleaningSchedule";
+        public static final String AVAILABLE_ROOMS = "/api/v1/availableRooms";
+        public static final String CREATE_BOOKING = "/api/v1/booking";
+        public static final String CLEANING_SCHEDULE = "/api/v1/cleaningSchedule";
     }
 }
